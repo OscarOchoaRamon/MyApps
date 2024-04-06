f89efa62fc692202bf18e35ac803e6f36070e200
OscarOchoaRamon <140092213+OscarOchoaRamon@users.noreply.github.com>
2024-04-05 23:07:25 -0500

    rosa-viento-app.py

--- rosa-viento-app.py
+++ rosa-viento-app.py
@@ -1,39 +1,37 @@
 from windrose import WindroseAxes
-from matplotlib import pyplot as plt
-import matplotlib.cm as cm
 import numpy as np
 import streamlit as st
 import pandas as pd
-import os
 
+st.title(":blue[Generación de rosa de viento]")
+st.write("_(Elaborado por el Equipo de Recursos Hídricos :droplet:)_")
 
-# uploaded_file = st.file_uploader("Choose a file")
-# if uploaded_file is not None:
-#     # To read file as bytes:
-#     bytes_data = uploaded_file.getvalue()
+#I stayed here
+if 'w' not in st.session_state:    
+    # Create wind speed and direction variables
+    st.session_state['w'] = np.random.random(500)
 
-#     # Can be used wherever a "file-like" object is accepted:
-#     dataframe = pd.read_csv(uploaded_file, delimiter=';')
-#     st.write(dataframe)
-
-st.title("Generación de Rosa de viento")
-
-# Create wind speed and direction variables
-ws = np.random.random(500) * 6
-wd = np.random.random(500) * 360
+division_angule = np.linspace(0, 2 * np.pi, 16, endpoint=False)
+directions = ['E', 'ENE', 'NE', 'NNE', 'N', 'NNW', 'NW', 'WNW','W', 'WSW', 'SW', 'SSW', 'S', 'SSE', 'SE', 'ESE']
 
 ax1 = WindroseAxes.from_ax()
-ax1.bar(wd, ws, normed=True, opening=0.8, edgecolor='white')
+ax1.bar(st.session_state['w']*360, st.session_state['w']*20, bins=8, normed=True, opening=0.8)
 ax1.set_legend()
+ax1.set_xticks(division_angule)
+ax1.set_xticklabels(directions)
 
 ax2 = WindroseAxes.from_ax()
-ax2.box(wd, ws, bins=np.arange(0, 8, 1))
+ax2.box(st.session_state['w']*360, st.session_state['w']*20, bins=8, normed=True)
 ax2.set_legend()
+ax2.set_xticks(division_angule)
+ax2.set_xticklabels(directions)
 
 ax3 = WindroseAxes.from_ax()
-ax3.contourf(wd, ws, bins=np.arange(0, 8, 1), cmap=cm.hot)
+ax3.contourf(st.session_state['w']*360, st.session_state['w']*20, bins=8, normed=True)
 ax3.set_legend()
-
+ax3.set_xticks(division_angule)
+ax3.set_xticklabels(directions)
+    
 col1, col2, col3 = st.columns(3)
 
 with col1:
@@ -47,25 +45,18 @@ with col3:
     st.header('Opción C')
     st.pyplot(ax3.figure)
     
-uploaded_file = st.sidebar.file_uploader("Choose a file")
+uploaded_file = st.sidebar.file_uploader(":blue[Seleccione su archivo Excel:]")
+
+station_number = st.sidebar.text_input(':blue[Coloque el número de estaciones:]')
     
 add_selectbox = st.sidebar.selectbox(
-    '¿Qué tipo de rosa de viento te gustaría generar?',
+    ':blue[¿Qué tipo de rosa de viento le gustaría generar?]',
     ('','A', 'B', 'C')
 )
 
-def save_df_to_folder(folder_path):
-    """Saves dataframe to the provided folder."""
-    if not os.path.isdir(folder_path):
-        st.error('The provided folder does not exist. Please provide a valid folder path.')
-        return
-    ruta2=folder_path.replace('\\', '/')
-    ruta3=ruta2.replace("'", "")
-    os.chdir(ruta3)
-    
-ruta = st.text_input('Coloque la ruta donde se guardarán las imágenes')
-if st.button('Save Dataframe'):
-    save_df_to_folder(ruta)
+station_label = []
+for label in range(1,int(station_number)+1):
+    station_label.append('estacion_{}'.format(label))
 
 if uploaded_file is not None:
     # To read file as bytes:
@@ -73,27 +64,31 @@ if uploaded_file is not None:
 
     # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file, delimiter=';')
-    st.write(dataframe)
+    st.subheader("Aquí se muestran los gráficos generados :point_down::")
+    station_display = st.selectbox('Escoja la estación para visualizar la rosa de viento:',station_label)
     
     if add_selectbox=='A':    
         ax4 = WindroseAxes.from_ax()
-        ax4.bar(dataframe['dv'], dataframe['vv'], normed=True, opening=0.8, edgecolor='white')
+        ax4.bar(dataframe['dv_{}'.format(station_label.index(station_display)+1)], dataframe['vv_{}'.format(station_label.index(station_display)+1)], normed=True, opening=0.8, edgecolor='white')
         ax4.set_legend()
-        plt.savefig('imagenA.png')
+        ax4.set_xticks(division_angule)
+        ax4.set_xticklabels(directions)
         st.pyplot(ax4.figure)
         
     elif add_selectbox=='B':
         ax5 = WindroseAxes.from_ax()
-        ax5.box(dataframe['dv'], dataframe['vv'], bins=np.arange(0, 8, 1))
+        ax5.box(dataframe['dv_{}'.format(station_label.index(station_display)+1)], dataframe['vv_{}'.format(station_label.index(station_display)+1)], bins=8, normed=True)
         ax5.set_legend()
-        plt.savefig('imagenB.png')
+        ax5.set_xticks(division_angule)
+        ax5.set_xticklabels(directions)
         st.pyplot(ax5.figure)
         
     elif add_selectbox=='C':
         ax6 = WindroseAxes.from_ax()
-        ax6.contourf(dataframe['dv'], dataframe['vv'], bins=np.arange(0, 8, 1), cmap=cm.hot)
+        ax6.contourf(dataframe['dv_{}'.format(station_label.index(station_display)+1)], dataframe['vv_{}'.format(station_label.index(station_display)+1)], bins=8, normed=True)
         ax6.set_legend()
-        plt.savefig('imagenC.png')
+        ax6.set_xticks(division_angule)
+        ax6.set_xticklabels(directions)
         st.pyplot(ax6.figure)
 
 
